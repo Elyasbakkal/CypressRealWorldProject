0e44662f2ed205cfda9f7a407917f13fe3cf093b
Username <user@Usernames-MBP.fios-router.home>
2020-06-25 19:55:30 -0400

    First Cypress Commit

---
+++ src/test/java/loginFuntionality/TestCase1.java
@@ -0,0 +1,45 @@
+package loginFuntionality;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	
+	
+	
+	@Test
+	public void loginWithValidCredentials() {
+		System.out.println("loged in");
+	}
+
+	@Test
+	public void loginWithInvalidCredentials() {
+		System.out.println("logging in faild");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/loginFuntionality/TestCase2.java
@@ -0,0 +1,25 @@
+package loginFuntionality;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	
+	
+	
+	
+	
+	@Test
+	public void anotherTestInTestCase2() {
+		System.out.println("sup bro");
+	}
+	
+	
+
+	@Test
+	public void helloWorld() {
+		System.out.println("sup sister");
+	}
+	
+	
+
+}
